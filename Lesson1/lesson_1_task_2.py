1e7c263bef89113721cf0819cde0acb1ac01cd8d
ddimonoff <ddimonoff@yandex.ru>
2025-11-21 22:59:36 +0400

    Lesson1

--- Lesson1/lesson_1_task_2.py
+++ Lesson1/lesson_1_task_2.py
@@ -1,4 +1,4 @@
 my_name = input ("Ваше имя:")
 my_age = input ("Ваш возраст:")
-my_age = input ("А через три года ?:")
+my_age = input ("А через три года :")
 print (my_name + ", через три года Вам будет уже " + my_age + "?")
