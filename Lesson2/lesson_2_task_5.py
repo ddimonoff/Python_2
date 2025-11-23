0d27585d371ba33f2afb51feba9855fa259bb878
ddimonoff <ddimonoff@yandex.ru>
2025-11-23 23:40:07 +0400

    Lesson2

---
+++ Lesson2/lesson_2_task_5.py
@@ -0,0 +1,11 @@
+month = input ("Введите номер месяца: ")
+season = int (month)
+if (season < 1) or (season > 12):
+    print("нет гакоого месяца в году")
+elif (season > 2) and (season < 6):
+    print("Весна")
+elif (season > 5) and (season < 9):
+    print("Лето")
+elif (season > 8) and (season < 12):
+    print("Осень")
+else: print ("Зима")
