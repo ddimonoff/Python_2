32ea414c0daa095fe2906f41bf75a1c23f436969
ddimonoff <ddimonoff@yandex.ru>
2025-11-24 20:12:18 +0400

    Lesson2

--- Lesson2/lesson_2_task_5.py
+++ Lesson2/lesson_2_task_5.py
@@ -1,7 +1,7 @@
 month = input ("Введите номер месяца: ")
 season = int (month)
 if (season < 1) or (season > 12):
-    print("нет гакоого месяца в году")
+    print("Нет гакоого месяца в году")
 elif (season > 2) and (season < 6):
     print("Весна")
 elif (season > 5) and (season < 9):
