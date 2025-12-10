e575a9946d58ff37a08ec8be2313861be0fe0b84
ddimonoff <ddimonoff@yandex.ru>
2025-12-10 19:20:56 +0400

    Lesson5

--- Lesson5/lesson05_task2.py
+++ Lesson5/lesson05_task2.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("http://uitestingplayground.com/dynamicid")
 sleep(2)
-
 blue_button = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
 blue_button.click()
 sleep(5)
