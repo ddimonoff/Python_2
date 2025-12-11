cd8616bebbf42f4fc5ccb2eb13be311de83e9651
ddimonoff <ddimonoff@yandex.ru>
2025-12-11 20:11:28 +0400

    Lesson6

---
+++ Lesson6/lesson6_task1.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("http://uitestingplayground.com/ajax")
+blue_button = driver.find_element(By.CSS_SELECTOR, "#ajaxButton")
+blue_button.click()
+driver.implicitly_wait(17)
+
+content = driver.find_element(By.CSS_SELECTOR, "#content")
+txt = content.find_element(By.CSS_SELECTOR, "p.bg-success").text
+
+print(txt)
+driver.quit()
---
+++ Lesson6/lesson6_task3.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+driver.implicitly_wait(20)
+picture = ""
+picture = driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src")
+print(picture)
+
+driver.quit()
